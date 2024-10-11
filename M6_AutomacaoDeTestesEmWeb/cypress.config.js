73601f018392424032ef08151c79508afdec061d
marcoslott <marcoslott@gmail.com>
2024-10-11 12:50:53 -0300

    ajustando a task utilizada no test24 (automation-exercise-with-pom.cy.js)

--- M6_AutomacaoDeTestesEmWeb/cypress.config.js
+++ M6_AutomacaoDeTestesEmWeb/cypress.config.js
@@ -1,7 +1,31 @@
 module.exports = {
+  // Outras configurações do Cypress
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        deleteFileIfExists(filePath) {
+          const fs = require('fs');
+
+          try {
+            // Verifica se o arquivo existe antes de tentar deletar
+            fs.accessSync(filePath, fs.constants.F_OK);
+            fs.unlinkSync(filePath);
+            console.log(`Arquivo deletado com sucesso: ${filePath}`);
+          } catch (error) {
+            if (error.code === 'ENOENT') {
+              console.log(`Arquivo ${filePath} não encontrado. Ignorando a deleção.`);
+            } else {
+              console.error(`Erro ao deletar arquivo: ${filePath}`, error);
+              // Outros tratamentos de erro
+            }
+          }
+
+          return null;
+        },
+      });
+
+      return config;
     },
-  },
+    downloadsFolder: "cypress/downloads",
+  }
 };
